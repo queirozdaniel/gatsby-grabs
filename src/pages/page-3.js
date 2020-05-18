e5809610a7cda9dc3d6181148e44859e8473fd92
queirozdaniel <danielqueirozak77@gmail.com>
2020-05-17 21:41:20 -0300

    Adicionando plugin para utilizar Markdown

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -1,10 +1,52 @@
 import React from 'react'
 import Layout from '../components/layout'
-import { Link } from 'gatsby'
+import { StaticQuery, Link, graphql } from 'gatsby'
+
+const getImageData = graphql`
+{
+    allFile {
+      edges {
+        node {
+          relativePath
+          size
+          extension
+        }
+      }
+    }
+}
+`
+
 
 export default () => (
     <Layout>
-        <h1>Ola mundo</h1>
+        <h1>Página 3</h1>
+        <h3>Info sobre imagens</h3>
+        <StaticQuery
+            query={getImageData}
+            render={data => (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Relative Path</th>
+                            <th>Size</th>
+                            <th>Extension</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.allFile.edges.map((edge, index) => (
+                            <tr key={index}>
+                                <td>{edge.node.relativePath}</td>
+                                <td>{edge.node.size}</td>
+                                <td>{edge.node.extension}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+            )}
+        />
+
+
         <Link to="/page-2">Go to page 2</Link>
     </Layout>
 )
---
+++ src/pages/post-one.md
@@ -0,0 +1,8 @@
+---
+    title: "Primeiro post"
+    date: "2020-05-17" 
+---
+
+# Primeiro post
+
+Oi mundo com Gatsby Js
